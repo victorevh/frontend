3ec818a2526dff44b1eebbee380186917686d87d
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-08-26 20:35:13 -0300

    style(recommendation): add tailwind styles in list

--- src/components/RecommendationList/RecommendationList.js
+++ src/components/RecommendationList/RecommendationList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RecommendationList({ recommendations, loading }) {
   return (
-    <div>
+    <div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md">
       <h2 className="text-lg font-bold mb-4">Lista de Recomendações:</h2>
 
       {loading && (

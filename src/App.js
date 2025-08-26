725ff5a846c0ff2b0b3ae682f4a14ba1be6320f8
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-08-26 20:48:05 -0300

    feat(app): add responsive layout

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   const { recommendations, getRecommendations, setRecommendations, loading } = useRecommendations(products);
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold mb-8">Recomendador de Produtos RD Station</h1>
-      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="col-span-2 mb-4">
-          <p className="text-lg">
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Recomendador de Produtos RD Station</h1>
+      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-5xl grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="md:col-span-2 mb-4">
+          <p className="text-lg text-gray-700">
             Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
           </p>
         </div>

499a788c79f1c2f3b72252a3a5682468fa7aca2d
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-08-26 11:40:04 -0300

    chore: initial commit with RD Station base project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Form from './components/Form/Form';
+import RecommendationList from './components/RecommendationList/RecommendationList';
 
 function App() {
+  const [recommendations, setRecommendations ] = useState([])
+
+  /**
+   * Dadas atualizações no formulário, necessário atualizar a lista de recomendações
+   */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-8">Recomendador de Produtos RD Station</h1>
+      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="col-span-2 mb-4">
+          <p className="text-lg">
+            Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
+          </p>
+        </div>
+        <div>
+          <Form />
+        </div>
+        <div>
+          <RecommendationList recommendations={recommendations} />
+        </div>
+      </div>
     </div>
   );
 }

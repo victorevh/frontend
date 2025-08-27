d5a6caff066bf8e32756c78598bd3ad451fcaa8c
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-08-26 21:20:25 -0300

    feat(products): use mock when running the application in dev

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import mockProducts from '../mocks/mockProducts';
 
 const baseURL = 'http://localhost:3001';
 
 const getProducts = async () => {
+  if (process.env.REACT_APP_USE_MOCK === 'true') {
+    return mockProducts;
+  }
+
   try {
     const response = await axios.get(`${baseURL}/products`);
     return response.data;
